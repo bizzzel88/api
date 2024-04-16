ea3b2d43af85d586e90357f7abbf9cf3204a87ee
bizzzel88 <166687022+bizzzel88@users.noreply.github.com>
2024-04-16 21:06:49 +0300

    py.py

--- py.py
+++ py.py
@@ -1,19 +1,10 @@
 import requests
 
-# URL для доступа к API Petstore
-url = "https://petstore.swagger.io/v2/pet/findByStatus"
+name = input("Введите имя для анализа национальности: ")
+url = f"https://api.nationalize.io?name={name}"
 
-# Параметры запроса
-params = {
-    "status": "available"
-}
+response = requests.get(url)
 
-# Отправка запроса
-response = requests.get(url, params=params)
+data = response.json()
 
-if response.status_code == 200:
-    pets = response.json()
-    for pet in pets:
-        print(f"Имя питомца: {pet.get('name')}, ID: {pet.get('id')}")
-else:
-    print("Не удалось получить список питомцев. Пожалуйста, попробуйте еще раз.")
+print(data)
